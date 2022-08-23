ae4c8e066d9026e8b7084739d302f22a8ebdb0f6
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2022-08-23 09:27:35 -0300

    Required Fields Added
    Adicionei campos required e retirei o comentário da Model dos Subjects

--- WebAPI/Models/SchoolSubject.cs
+++ WebAPI/Models/SchoolSubject.cs
@@ -1,12 +1,13 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace WebAPI.Models
 {
     public class SchoolSubject
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The Name field is required")]
         public string Name { get; set; }
         public double Test_Score { get; set; }
-        //public Student Student { get; set; }
     }
 }
--- WebAPI/Models/Student.cs
+++ WebAPI/Models/Student.cs
@@ -8,13 +8,13 @@ namespace WebAPI.Models
     {
         [Key]
         public Guid Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The FirstName field is required")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The LastName field is required")]
         public string LastName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Login field is required")]
         public string Login { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Password field is required")]
         private string _Password;
         public string Password
         {
@@ -28,28 +28,28 @@ namespace WebAPI.Models
                     _Password = BCrypt.Net.BCrypt.HashPassword(value);
             }
         }
-        [Required]
+        [Required(ErrorMessage = "The Email field is required")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The CPF field is required")]
         public string CPF { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Address field is required")]
         public string Address { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The PhoneNumber field is required")]
         public string PhoneNumber { get; set; }
         public string AddedIn { get; set; } = DateTime.Now.ToString("dd/MM/yyyy");
         public DateTime? addedInEdited
         {
             get { return DateTime.Parse(AddedIn, new CultureInfo("pt-BR")); }
         }
-        [Required]
+        [Required(ErrorMessage = "The BirthDate field is required")]
         public string BirthDate { get; set;}
         public DateTime? dateOfBirth
         {
             get { return DateTime.Parse(BirthDate, new CultureInfo("pt-BR")); }
         }
-        [Required]
+        [Required(ErrorMessage = "The Education Level field is required")]
         public string educationLevel { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Grade field is required")]
         public string Grade { get; set; }
         public ICollection<SchoolSubject> Subjects { get; set; }
     }
